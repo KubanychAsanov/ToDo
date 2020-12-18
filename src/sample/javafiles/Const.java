cc1c9473fb1d3048c72f8cdec73a07b9f6688337
Kuba <kuba.asanov4@gmail.com>
2020-12-18 17:36:00 +0600

    Initial commit

---
+++ src/sample/javafiles/Const.java
@@ -0,0 +1,10 @@
+package sample.javafiles;
+
+public class Const {
+    public static final String USER_TABLE = "users";
+    public static final String USERS_FIRSTNAME = "first_name";
+    public static final String USERS_LASTNAME = "last_name";
+    public static final String USERS_LOGIN = "user_login";
+    public static final String USERS_PASS = "user_pass";
+    public static final String USERS_GENDER = "user_gender";
+}
